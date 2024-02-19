b03ad032958d1a493996f6e38265cd5fea8bacac
meddlin <drushing.dev@gmail.com>
2024-02-18 23:08:18 -0600

    helpful constructors

--- EveryBudgetApi/ViewModels/BudgetViewModel.cs
+++ EveryBudgetApi/ViewModels/BudgetViewModel.cs
@@ -1,3 +1,5 @@
+using EveryBudgetCore.Models;
+
 namespace EveryBudgetApi.ViewModels;
 
 public class BudgetViewModel
@@ -6,4 +8,28 @@ public class BudgetViewModel
     public DateTime DateUpdated { get; set; }
 
     public List<CategoryViewModel> Categories { get; set; }
+
+    /// <summary>
+    /// Empty constructor
+    /// </summary>
+    public BudgetViewModel() { }
+
+    /// <summary>
+    /// Constructor - to convert a collection of <c>Category</c> objects
+    /// </summary>
+    /// <param name="categories"></param>
+    public BudgetViewModel(IEnumerable<Category> categories)
+    {
+        var vm = new BudgetViewModel();
+
+        foreach (var category in categories)
+        {
+            vm.Categories.Add(new CategoryViewModel(category));
+        }
+    }
+
+    public BudgetViewModel(IEnumerable<CategoryViewModel> categories)
+    {
+        Categories = categories.ToList();
+    }
 }
